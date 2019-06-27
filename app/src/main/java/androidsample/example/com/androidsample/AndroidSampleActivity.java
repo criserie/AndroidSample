6ed254ddb661a029defeec1c15300cbd96b992ab
Cris Skrabut <criserie@hotmail.com>
2019-06-27 15:08:28 -0700

    Desktop AndroidSample Commit

--- app/src/main/java/androidsample/example/com/androidsample/AndroidSampleActivity.java
+++ app/src/main/java/androidsample/example/com/androidsample/AndroidSampleActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class AndroidSampleActivity extends AppCompatActivity {
+    int messagenum = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +32,32 @@ public class AndroidSampleActivity extends AppCompatActivity {
             }
         });
         final TextView myView = findViewById(R.id.textView);
-        final Button button = findViewById(R.id.button_id);
+        Button button = findViewById(R.id.button_id);
+        final SmsManager smsManager = SmsManager.getDefault();
+
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if(!myView.getText().equals("changed"))
                     myView.setText("changed");
                 else
                     myView.setText("Changed Back!!!");
+
+                while(messagenum <= 7)
+                {
+                    int breakTimer = 0;
+                    try {
+                        smsManager.sendTextMessage("520-881-0062", null, "I just built an app that will send you this message 7 times, this is Text " + messagenum + " of 7.", null, null);
+                    } catch (IllegalArgumentException ie) {
+                        myView.setText("IllegalArgumentException Thrown");
+                    }
+
+                    while (breakTimer < 1000000000)
+                    {
+                        breakTimer++;
+                    }
+                    messagenum++;
+                }
 
             }
         });
